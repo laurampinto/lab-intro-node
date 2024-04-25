c6a0651eab0ec1bf283f9b0deb1ef61da9f3a3dd
Laura Pinto <laurampinto90@gmail.com>
2024-04-25 19:33:19 +0100

    Done except bonus iterations

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let index = 0;
+    for (index = 0; index < this.length; index++) {
+      if (this.items[index] > item) {
+        break;
+      }
+    }
+    this.items.splice(index, 0, item);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let maxValue = this.items[0];
+    for (let i = 0; i < this.length; i++) {
+      if (this.items[i] > maxValue) {
+        maxValue = this.items[i];
+      }
+    }
+    return maxValue;
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("Empty SortedList");
+    }
+    let minValue = this.items[0];
+    for (let i = 0; i < this.length; i++) {
+      if (this.items[i] < minValue) {
+        minValue = this.items[i];
+      }
+    }
+    return minValue;
+  }
 
   sum() {}
 
